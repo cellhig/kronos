cda64d3aeaf67f0af69d187f8f689877387857a1
cellhig <higomez7@misena.edu.co>
2016-03-13 15:38:40 -0500

    buscador global Empleado
    -Se implemento Buscador global para la tabla empleado.

--- backend/views/empleado/index.php
+++ backend/views/empleado/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="empleado-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Registrar Empleado', ['create'], ['class' => 'btn btn-success']) ?>
@@ -48,12 +48,12 @@ $this->params['breadcrumbs'][] = $this->title;
         'fontAwesome' => true,
         'selectedColumns'=> [1, 2, 3, 4, 5],  // Col seq 2 to 6
         'columnSelectorOptions'=>[
-            'label' => 'Columns'],
+            'label' => 'Columnas'],
         'hiddenColumns'=>[0, 4, 9], // SerialColumn, Color, & ActionColumn
         'disabledColumns'=>[0,0], // ID & Name
         'noExportColumns'=>[6], // Status
         'dropdownOptions' => [
-            'label' => 'Export All',
+            'label' => 'Exportar A..',
             'class' => 'btn btn-default'
         ],
     ]);
@@ -62,18 +62,43 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        //'filterModel' => $searchModel,//se implementa un bucador global por eso ya no es necesario
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
             //'id',
+            /*[
+                'attribute' => 'persona_id',
+                'value' => 'persona.nombre',
+                'label' => 'Nombre',
+            ],*/
             'persona.nombre',
+            /*[
+                'attribute' => 'persona_id',
+                'value' => 'persona.apellido',
+                'label' => 'Apellido',
+            ],*/
             'persona.apellido',
+            /*[
+                'attribute' => 'persona_id',
+                'value' => 'persona.direccion',
+                'label' => 'Direccion',
+            ],*/
             'persona.direccion',
+            /*[
+                'attribute' => 'persona_id',
+                'value' => 'persona.telefono',
+                'label' => 'Telefono',
+            ],*/
             'persona.telefono',
             //'telefono_movil',
             //'estado',
             //'persona_id',
+            /*[
+                'attribute' => 'cargo_id',
+                'value' => 'cargo.nombre_cargo',
+                'label' => 'Cargo',
+            ],*/
             'cargo.nombre_cargo',
             // 'sede_id',
 
